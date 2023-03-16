8f4190e342f5ee7426db3fe259e43bb1e09898a4
gasanchik <46112415+gasanchik@users.noreply.github.com>
2023-03-16 22:58:45 +0100

    Update vector2.ts

--- vector2.ts
+++ vector2.ts
@@ -1,1 +1,22 @@
+type Vector2 = {
+    x: number,
+    y: number
+}
 
+namespace Vector2Library {
+    export function add(vector1: Vector2, vector2: Vector2): Vector2 {
+        return {}
+    }
+
+    export function unit(vector: Vector2): Vector2 {
+        return {}
+    }
+
+    export function clone(vector: Vector2): Vector2 {
+        return {}
+    }
+
+    export function logVector(vector: Vector2): Vector2 {
+        return {}
+    }
+}
